b279e16f02f03ec3d75e5576b23927ccf92cea73
Jose David <asefadoj@gmail.com>
2025-01-21 22:31:17 -0300

    Iniciando el challenge

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array para almacenar los nombres de amigos
+let amigos = [];
+
+
